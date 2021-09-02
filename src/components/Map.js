95ce66208ee3accf129a94a5d92299cef65ab7a4
Paritosh7 <paritosh7.sahni@gmail.com>
2021-09-03 04:25:15 +0530

    init Leaflet Maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,24 @@
+import "../css/Map.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 export const Map = () => {
   return (
     <div id="bottom-half-map">
-      <h1>I am MAP</h1>
+      <MapContainer
+        center={[30.3244, 78.03392]}
+        zoom={11}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[30.3244, 78.03392]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
